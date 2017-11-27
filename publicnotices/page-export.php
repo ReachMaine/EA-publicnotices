85721f9cb14b4a21566a47ea3dd77b20b9f99fd4
linda@reachmaine.com <linda@reachmaine.com>
2017-11-27 10:17:46 -0500

    baby step 3

--- publicnotices/page-export.php
+++ publicnotices/page-export.php
@@ -6,36 +6,40 @@ Template name: Export Notices
 * version: 1.0
 * based on flatsome page-right-sidebar page template.
 */
+
 if ( 'GET' == $_SERVER['REQUEST_METHOD'] && !empty( $_GET['action'] ) && $_GET['action'] == 'getexport' ) {
     $indate = $_GET['targetday'];
 }
 
 function get_public_notices ($in_date_str) {
   $out_html = "<p>yep. Yep.</p>";
-  global $wpdb;
+  //global $wpdb;
 	$in_date_date = new DateTime($in_date_str);
 	$datestr = $in_date_date->format("Ymd");
-	/* $sqlreq = 'SELECT CONCAT("<notice><subcategory_id>17</subcategory_id><date>",
+ $sqlreq = 'SELECT CONCAT("<notice><subcategory_id>17</subcategory_id><date>",
 							DATE_FORMAT(cast(post_date AS DATE),"%m/%d/%Y"),
 							"</date><text>",
               post_title,
               post_content,"</text></notice>")
-              FROM ea_13_posts WHERE cast(post_date AS DATE) = cast("'.$datestr.'" as date) and post_status in ("publish", "future") and post_type = "post" limit 100'; */
+              FROM ea_13_posts WHERE cast(post_date AS DATE) = cast("'.$datestr.'" as date) and post_status in ("publish", "future") and post_type = "post" limit 100';
   $sqlreq = 'SELECT DATE_FORMAT(cast(post_date AS DATE),"%m/%d/%Y"), post_title, post_content FROM ea_13_posts WHERE cast(post_date AS DATE) = cast("'.$datestr.'" as date) and post_status in ("publish", "future") and post_type = "post" limit 100';
-	$sqlresult = $wpdb->get_results($sqlreq);
+  $out_html .= $sqlreq;
+	//$sqlresult = $wpdb->get_results($sqlreq);
   //echo"<pre>";  var_dump($sqlresult);  echo "</pre>";
-  foreach ($sqlresult as $pnotice) {
+  /* foreach ($sqlresult as $pnotice) {
      $post_text = htmlspecialchars($pnotice->post_content, ENT_QUOTES);
      $schar = array("ë", "§", "©", "•","●","—","–")
     $rchar = array("&euml;","&sect;", "&copy;", "&#8226;", "&#8226;". "&mdash;", "&ndash;");
     $post_text = str_replace($schar, $rchar, $post_text);
 
-      $out_html .= '<notice><subcategory_id>17</subcategory_id>';
-      $out_html .= '<date>'.post_date.'</date>';
-      $out_html .= $pnotice->post_title;
-      $out_html .= $post_text;
-      $out_html .= '</notice>';
-  } // end for
+    //  $out_html .= '<notice><subcategory_id>17</subcategory_id>';
+    //  $out_html .= '<date>'.post_date.'</date>';
+      $out_html .= $pnotice->post_title.' ';
+    //  $out_html .= $post_text;
+    //  $out_html .= '</notice>';
+  } // end for */
+
+  $out_html .= "<p>did the sql2...</p>";
   return $out_html;
 }
 
@@ -64,9 +68,11 @@ get_header(); ?>
 		<div id="export_notices">
       <p>here.</p>
 			<?php if ($indate) {
-        echo "<p>";
-				echo get_public_notices($indate);
-        echo "</p>";
+        echo "<p>got date</p>";
+				$pnotices =  get_public_notices($indate);
+        echo $pnotices;
+        //echo"<pre>";  var_dump($pnotices);  echo "</pre>";
+        echo "<p>after get.. </p>";
 			} ?>
 		</div>
 
